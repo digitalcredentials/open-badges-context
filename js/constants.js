5ee9629cb881a82e1ed20fd9b8621a3e52c1b4aa
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-05-25 09:07:18 -0400

    Add additional context url aliases.

--- js/constants.js
+++ js/constants.js
@@ -1,9 +1,11 @@
+/* eslint-disable */
 /*!
  * Copyright (c) 2022 Digital Credentials Consortium. All rights reserved.
  */
 'use strict';
 
 module.exports = {
-  CONTEXT_URL_V3:
-    'https://imsglobal.github.io/openbadges-specification/ob_v3p0.html'
+  CONTEXT_URL_V3: 'https://w3id.org/openbadges/v3',
+  CONTEXT_URL_V3_JFF_V1: 'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/jff-vc-edu-plugfest-1-context.json',
+  CONTEXT_URL_V3_IMS: 'https://purl.imsglobal.org/spec/ob/v3p0/context'
 };

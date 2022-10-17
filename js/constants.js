9e81479fe3c771ff1decc3096bb0bd8cedb50d37
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-10-17 19:38:04 -0400

    Update IMS global obv3 context to latest spec.

--- js/constants.js
+++ js/constants.js
@@ -7,5 +7,5 @@
 module.exports = {
   CONTEXT_URL_V3: 'https://w3id.org/openbadges/v3',
   CONTEXT_URL_V3_JFF_V1: 'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/jff-vc-edu-plugfest-1-context.json',
-  CONTEXT_URL_V3_IMS: 'https://purl.imsglobal.org/spec/ob/v3p0/context'
+  CONTEXT_URL_V3_IMS: 'https://purl.imsglobal.org/spec/ob/v3p0/context.json'
 };

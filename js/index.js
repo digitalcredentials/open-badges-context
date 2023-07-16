b964f936ed1b20ee967dd320165d8e347e8d9838
Dmitri Zagidulin <dzagidulin@gmail.com>
2023-07-16 16:38:42 -0400

    Update context exports from 3.0-beta to 3.0.2

--- js/index.js
+++ js/index.js
@@ -1,22 +1,60 @@
+/* eslint-disable */
 /*!
  * Copyright (c) 2022 Digital Credentials Consortium. All rights reserved.
  */
 'use strict';
 
-const context = require('./context');
-const constants = require('./constants');
-const {CONTEXT_URL_V3, CONTEXT_URL_V3_JFF_V1, CONTEXT_URL_V3_IMS} = constants;
+/**
+ * Beta version of OBv3 context.
+ * Used by the OBv3 WG (by IMS Global, renamed to 1EdTech) until Nov 7, 2022.
+ * Documents processed using the OBv3 BETA context expanded terms to:
+ * https://imsglobal.github.io/openbadges-specification/ob_v3p0.html
+ * These terms were used in JFF Plugfests 1 and 2, and early DCC pilot deployments.
+ */
+const context_v3_beta = require('./contexts/context-3.0-beta');
+
+/**
+ * Finalized OBv3 "production" context, used after Nov 7, 2022.
+ * Documents processed using the final 3.0 context expanded terms to:
+ * https://purl.imsglobal.org/spec/vc/ob/vocab.html
+ */
+const context_v3_0_0 = require('./contexts/context-3.0');
+
+const context_v3_0_1 = require('./contexts/context-3.0.1');
+const context_v3_0_2 = require('./contexts/context-3.0.2');
+
+const CONTEXT_URL_V3_ALPHA =
+  'https://imsglobal.github.io/openbadges-specification/ob_v3p0.html';
+const CONTEXT_URL_V3_JFF_V1 =
+  'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/jff-vc-edu-plugfest-1-context.json';
+const CONTEXT_URL_V3_BETA =
+  'https://purl.imsglobal.org/spec/ob/v3p0/context.json';
+const CONTEXT_URL_V3_0_0 =
+  'https://purl.imsglobal.org/spec/ob/v3p0/context.json';
+const CONTEXT_URL_V3_0_1 =
+  'https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.1.json';
+const CONTEXT_URL_V3_0_2 =
+  'https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.2.json';
 
 const contexts = new Map();
-contexts.set(CONTEXT_URL_V3, context);
-contexts.set(CONTEXT_URL_V3_JFF_V1, context);
-contexts.set(CONTEXT_URL_V3_IMS, context);
+contexts.set(CONTEXT_URL_V3_ALPHA, context_v3_beta);
+contexts.set(CONTEXT_URL_V3_JFF_V1, context_v3_beta);
+contexts.set(CONTEXT_URL_V3_BETA, context_v3_beta);
+contexts.set(CONTEXT_URL_V3_0_0, context_v3_0_0);
+contexts.set(CONTEXT_URL_V3_0_1, context_v3_0_1);
+contexts.set(CONTEXT_URL_V3_0_2, context_v3_0_2);
+
+// Latest OBv3 context url
+const CONTEXT_URL_V3 = CONTEXT_URL_V3_0_2;
+contexts.set(CONTEXT_URL_V3, context_v3_0_2);
 
 module.exports = {
-  constants,
   contexts,
-  CONTEXT_URL_V3,
+  CONTEXT_URL_V3_ALPHA,
   CONTEXT_URL_V3_JFF_V1,
-  CONTEXT_URL_V3_IMS,
-  CONTEXT_V3: context
+  CONTEXT_URL_V3_BETA,
+  CONTEXT_URL_V3_0_0,
+  CONTEXT_URL_V3_0_1,
+  CONTEXT_URL_V3_0_2,
+  CONTEXT_URL_V3,
 };
